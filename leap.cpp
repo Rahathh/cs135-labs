803c4806f5b856084da47bf9f935abcb0ba4f1aa
Rahathh <116780364+Rahathh@users.noreply.github.com>
2024-08-28 12:34:56 -0400

    leap.cpp

--- leap.cpp
+++ leap.cpp
@@ -11,6 +11,7 @@ because this is the remainder of 37 when divided by 10.
 
 */
 
+
 #include <iostream>
 using namespace std;
 
@@ -18,8 +19,14 @@ int main() {
     int year;
     cout << "Enter year: ";
     cin >> year;
-    if (year % 4 != 0 && year % 400 != 0 || year % 100 == 0) {
-        cout << "Common Year";
+    if (year % 4 != 0) {
+        cout << "Common year";
+    }
+    else if (year % 100 != 0) {
+        cout << "Leap year";
+    }
+    else if (year % 400 != 0) {
+        cout << "Common year";
     }
     else {
         cout << "Leap year";
