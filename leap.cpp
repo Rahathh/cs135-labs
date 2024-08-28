1eb9446cf454ee26738adc7365b614d81cb65695
Rahathh <116780364+Rahathh@users.noreply.github.com>
2024-08-28 12:31:23 -0400

    leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,28 @@
+/*
+Author: Rahat Hassan
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab1C
+
+
+In C++, operator % computes the remainder of the 
+division of x by y. For example, 37 % 10 returns 7, 
+because this is the remainder of 37 when divided by 10.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+    cout << "Enter year: ";
+    cin >> year;
+    if (year % 4 != 0 && year % 400 != 0 || year % 100 == 0) {
+        cout << "Common Year";
+    }
+    else {
+        cout << "Leap year";
+    }
+    return 0;
+}
