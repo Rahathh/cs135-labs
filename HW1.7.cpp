efb4891f95d2995720709d0cbbed5e95bc6be6cd
Rahathh <116780364+Rahathh@users.noreply.github.com>
2024-09-07 14:48:51 -0400

    HW1.7.cpp

--- HW1.7.cpp
+++ HW1.7.cpp
@@ -1,4 +1,13 @@
+/*
+Author: Rahat Hassan
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: HW 1.7
 
+
+Writing 3 names and printing each of them, everyone one in a new line
+
+*/
 #include <iostream>
 using namespace std;
 
