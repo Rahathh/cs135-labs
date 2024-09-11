dd7a59f59eb5a521a421868e95345fa8521b9402
Rahathh <116780364+Rahathh@users.noreply.github.com>
2024-09-10 20:03:28 -0400

    print-interval.cpp

--- print-interval.cpp
+++ print-interval.cpp
@@ -1,3 +1,17 @@
+
+/*
+Author: Rahat Hassan
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab2B
+
+
+Write a program print-interval.cpp that asks the user to input two integers L and U (representing the lower and upper limits of the interval), 
+and print out all integers in the range L ≤ i < U separated by spaces. 
+Notice that we include the lower limit, but exclude the upper limit.
+
+*/
+
 #include <iostream>
 using namespace std;
 
