c6a3e1735397c0168c35a024e8c622f93de82f25
Rahathh <116780364+Rahathh@users.noreply.github.com>
2024-09-29 16:23:51 -0400

    box.cpp

---
+++ box.cpp
@@ -0,0 +1,35 @@
+/*
+
+Author: Rahat Hassan
+
+Course: CSCI-135
+
+Instructor: Tong Yi
+
+Assignment:Lab 4a
+
+
+Write a program 
+
+Write a program box.cpp that asks the user to input width and height and prints a solid rectangular box of the requested size using asterisks.
+
+Also, print a line Shape: between user input and the printed shape (to separate input from output).
+
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int width, height, count;
+    cout << "Input width: ";
+    cin >> width;
+    cout << "Input height: ";
+    cin >> height;
+    for (int n = 0; n < height; n++) {
+        for (int i = 0; i < width; i++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
